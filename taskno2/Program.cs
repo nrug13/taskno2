6c482bf97c4d5ccb95e2881fec788b83dee7c33a
Nurgun Ganbarova <nurgunganbarova@outlook.com>
2022-03-18 19:20:10 +0400

    Add project files.

---
+++ taskno2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace taskno2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // eded 3-e bolunen cutdur, 3- e bolunen tekdir
+            int a;
+            a = 24;
+            if (a % 3 == 0 && a % 2 == 0)
+            { Console.WriteLine(" 3-e bolunur cutdur"); }
+            else if (a % 3 == 0 && a % 2 == 1)
+            { Console.WriteLine("3-e bolunen tek ededdir"); }
+            else { Console.WriteLine("3-e bolunmur"); }
+        }
+    }
+}
